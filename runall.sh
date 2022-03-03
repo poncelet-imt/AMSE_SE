2049c859481e3816c1902ff75507f4e3ce1f2a68
poncelet-imt <thibault.poncelet@etu.imt-nord-europe.fr>
2022-03-04 00:26:01 +0100

    update for final simulation

--- runall.sh
+++ runall.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 binDir="./bin/"
 
 resultDir="./result/"
@@ -10,38 +11,48 @@ make all
 touch $odometryFile | cat >$odometryFile
 touch $kinematicsFile | cat >$kinematicsFile
 
+PI="3.14"
+
+
 K="0.009"
 I="13.5"
 Te="0.01"
 tMax="5.0"
 
-$binDir"ResetState" L
+$binDir"ResetState" L >> /dev/null
+$binDir"ResetState" R >> /dev/null
 
-$binDir"DCMotor" L 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te $tMax >> /dev/null &
+$binDir"DCMotor" L 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te >> /dev/null &
 pidDCMotor1=$!
-$binDir"DCMotor" R 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te $tMax >> /dev/null &
-pidDCMotor1=$!
+$binDir"DCMotor" R 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te >> /dev/null &
+pidDCMotor2=$!
 
-$binDir"RegPID" L $K $I 0.0 $Te $tMax > /dev/null &
+$binDir"RegPID" L $K $I 0.0 $Te > /dev/null &
 pidRegPID1=$!
-$binDir"RegPID" R $K $I 0.0 $Te $tMax > /dev/null &
+$binDir"RegPID" R $K $I 0.0 $Te > /dev/null &
 pidRegPID2=$!
 
-$binDir"Kinematics" 0.4 0.007 $Te $tMax >> $kinematicsFile &
+$binDir"Kinematics" 0.4 0.007 $Te >> $kinematicsFile &
 pidKinematics=$!
 
-$binDir"Odometry" $Te $tMax >> $odometryFile &
+$binDir"Odometry" $Te >> $odometryFile &
 pidOdometry=$!
 
-$binDir"SetTv" L 2.0 &
-$binDir"SetTv" R 1.0 &
+sh ./program.sh &
+pidProgram=$!
 
 
-wait $pidDCMotor1 2> /dev/null
-wait $pidDCMotor2 2> /dev/null
-wait $pidRegPID1 2> /dev/null
-wait $pidRegPID2 2> /dev/null
-wait $pidKinematics 2> /dev/null
-wait $pidOdometry 2> /dev/null
+wait $pidProgram
+
+sleep 2
+
+kill -10 $pidDCMotor1
+kill -10 $pidDCMotor2
+kill -10 $pidRegPID1
+kill -10 $pidRegPID2
+kill -10 $pidKinematics
+kill -10 $pidOdometry
+
+python3 ./graphPloter.py $odometryFile --xy
 
 python3 ./graphPloter.py $odometryFile
