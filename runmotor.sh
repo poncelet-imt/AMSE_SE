07ed7aa1f4c167b51f7af107d0bc9672224bc46a
poncelet-imt <thibault.poncelet@etu.imt-nord-europe.fr>
2022-03-03 20:02:22 +0100

    discard output of DCMotor

--- runmotor.sh
+++ runmotor.sh
@@ -15,12 +15,12 @@ tMax="0.5"
 
 $binDir"ResetState" L
 
-$binDir"DCMotor" L 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te $tMax >temp &
-pid_DCMotor = $!
+$binDir"DCMotor" L 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 $Te $tMax > /dev/null &
+pidDCMotor = $!
 $binDir"RegPID" L $K $I 0.0 $Te $tMax >> $resultFile &
-pid_RegPID = $!
+pidRegPID = $!
 $binDir"SetTv" L 1.0 &
 
-wait $pid_DCMotor
-wait $pid_RegPID
+wait $pidDCMotor
+wait $pidRegPID
 python3 ./graphPloter.py $resultFile 
