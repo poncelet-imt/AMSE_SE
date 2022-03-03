e1cc17ef2d45f755b52e6e8d2dfb2824f31da9d2
poncelet-imt <thibault.poncelet@etu.imt-nord-europe.fr>
2022-03-03 13:26:05 +0100

    formating name

--- runall.sh
+++ runall.sh
@@ -1,9 +1,9 @@
 
 make all
 
-fileMissing=false
-
 binDir="./bin/"
+
+resultDir="./result/"
 
 $binDir"DCMotor" L 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 0.01 &
 $binDir"DCMotor" R 1.8 0.02 0.004 0.02 3.2e-5 6.5e-6 0.01 &
@@ -13,4 +13,4 @@ $binDir"RegPID" R 0.02 16 0.0 0.01 &
 
 $binDir"Kinematics" 0.4 0.007 0.01 &
 
-$binDir"Odometry" 0.01 &
+$binDir"Odometry" 0.01 >> $resultDir"data.csv"
